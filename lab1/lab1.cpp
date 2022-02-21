d2ed0d90b638b358e0d80bfe495ade784e7db764
Ayrot2 <91112347+Ayrot2@users.noreply.github.com>
2022-02-22 02:37:48 +0300

    Update lab1.cpp

--- lab1/lab1.cpp
+++ lab1/lab1.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 #include <cstdlib>
 #include <time.h>
 using namespace std;
@@ -9,10 +9,11 @@ bool neighborhood(int n, int x1, int x2, int y1, int y2);
 bool border(int n, int x, int y, int hight, int width);
 int death_counter(int n, bool m_death[]);
 
+
 int main()
 {
     srand(time(NULL));
-    int width, hight, n,death;
+    int width, hight, n, death;
     cin >> width;
     cin >> hight;
     cout << '\n';
@@ -23,11 +24,11 @@ int main()
     char* map = new char[width * hight]; // Отображение на экране;
     bool* m_death = new bool[n]; //Если клетка живая, то элемент массива - true; иначе false.
 
-    map_creation(n, width, hight,coord_x,coord_y,map,m_death);
-    cout << iteration_of_moving(n,death, width, hight,coord_x,coord_y,map,m_death);
+    map_creation(n, width, hight, coord_x, coord_y, map, m_death);
+    cout << iteration_of_moving(n, death, width, hight, coord_x, coord_y, map, m_death);
 }
 
-void map_creation(int n, int width, int hight, int coord_x[], int coord_y[], char map[],bool m_death[]) {
+void map_creation(int n, int width, int hight, int coord_x[], int coord_y[], char map[], bool m_death[]) {
 
 
     for (int i = 0; i < width * hight; i++)
@@ -50,13 +51,13 @@ void map_creation(int n, int width, int hight, int coord_x[], int coord_y[], cha
     cout << " " << '\n';
 }
 
-int iteration_of_moving(int n, int death, int width, int hight,int coord_x[],int coord_y[],char map[],bool m_death[]) {
+int iteration_of_moving(int n, int death, int width, int hight, int coord_x[], int coord_y[], char map[], bool m_death[]) {
     int count = 0;
     while (death < n) {
         death = 0;
         for (int i = 0; i < n; i++)
             for (int j = 0; j < n; j++)
-                if (neighborhood(n, coord_x[i], coord_x[j], coord_y[i], coord_y[j])==true) {
+                if (neighborhood(n, coord_x[i], coord_x[j], coord_y[i], coord_y[j]) == true) {
                     map[coord_y[i] * width + coord_x[i]] = 'X';
                     map[coord_y[j] * width + coord_x[j]] = 'X'; //означает, что клетка умерла.
                     m_death[i] = m_death[j] = true;
@@ -68,38 +69,57 @@ int iteration_of_moving(int n, int death, int width, int hight,int coord_x[],int
             }
         }
 
-        death = death_counter(n,m_death);
+        death = death_counter(n, m_death);
 
         for (int i = 0; i < n; i++) {
             if (m_death[i] == false) {
-                int vect = rand() % 4;
-                if (vect == 0) {
-                    map[coord_y[i] * width + coord_x[i]] = '*';
-                    coord_y[i] -= 1;
-                    if (map[coord_y[i] * width + coord_x[i]] == '@')
-                        coord_y[i] += 1;
-                    map[coord_y[i] * width + coord_x[i]] = '@';
-                }
-                if (vect == 1) {
-                    map[coord_y[i] * width + coord_x[i]] = '*';
-                    coord_x[i] += 1;
-                    if (map[coord_y[i] * width + coord_x[i]] == '@')
-                        coord_x[i] -= 1;;
-                    map[coord_y[i] * width + coord_x[i]] = '@';
-                }
-                if (vect == 2) {
-                    map[coord_y[i] * width + coord_x[i]] = '*';
-                    coord_y[i] += 1;
-                    if (map[coord_y[i] * width + coord_x[i]] == '@')
+                if (hight != 1) {
+                    int vect = rand() % 4;
+                    if (vect == 0) {
+                        map[coord_y[i] * width + coord_x[i]] = '*';
                         coord_y[i] -= 1;
-                    map[coord_y[i] * width + coord_x[i]] = '@';
-                }
-                if (vect == 3) {
-                    map[coord_y[i] * width + coord_x[i]] = '*';
-                    coord_x[i] -= 1;
-                    if (map[coord_y[i] * width + coord_x[i]] == '@')
+                        if (map[coord_y[i] * width + coord_x[i]] == '@')
+                            coord_y[i] += 1;
+                        map[coord_y[i] * width + coord_x[i]] = '@';
+                    }
+                    if (vect == 1) {
+                        map[coord_y[i] * width + coord_x[i]] = '*';
                         coord_x[i] += 1;
-                    map[coord_y[i] * width + coord_x[i]] = '@';
+                        if (map[coord_y[i] * width + coord_x[i]] == '@')
+                            coord_x[i] -= 1;;
+                        map[coord_y[i] * width + coord_x[i]] = '@';
+                    }
+                    if (vect == 2) {
+                        map[coord_y[i] * width + coord_x[i]] = '*';
+                        coord_y[i] += 1;
+                        if (map[coord_y[i] * width + coord_x[i]] == '@')
+                            coord_y[i] -= 1;
+                        map[coord_y[i] * width + coord_x[i]] = '@';
+                    }
+                    if (vect == 3) {
+                        map[coord_y[i] * width + coord_x[i]] = '*';
+                        coord_x[i] -= 1;
+                        if (map[coord_y[i] * width + coord_x[i]] == '@')
+                            coord_x[i] += 1;
+                        map[coord_y[i] * width + coord_x[i]] = '@';
+                    }
+                }
+                else {
+                    int vect = rand() % 2;
+                    if (vect == 0) {
+                        map[coord_y[i] * width + coord_x[i]] = '*';
+                        coord_x[i] -= 1;
+                        if (map[coord_y[i] * width + coord_x[i]] == '@')
+                            coord_x[i] += 1;
+                        map[coord_y[i] * width + coord_x[i]] = '@';
+                    }
+                    if (vect == 1) {
+                        map[coord_y[i] * width + coord_x[i]] = '*';
+                        coord_x[i] += 1;
+                        if (map[coord_y[i] * width + coord_x[i]] == '@')
+                            coord_x[i] -= 1;;
+                        map[coord_y[i] * width + coord_x[i]] = '@';
+                    }
                 }
             }
         }
@@ -116,20 +136,27 @@ int iteration_of_moving(int n, int death, int width, int hight,int coord_x[],int
 }
 
 bool neighborhood(int n, int x1, int x2, int y1, int y2) { //если две живые клекти находятся по-соседству
-    if ((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2) == 1) { 
+    if ((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2) == 1) {
         return true;
     }
 }
 
 bool border(int n, int x, int y, int hight, int width) { //Если живая клетка добралась до границы
-    if (x * y == 0 || x == width - 1 || y == hight - 1) {
-        return true;
+    if (hight != 1) {
+        if (x * y == 0 || x == width - 1 || y == hight - 1) {
+            return true;
+        }
+    }
+    else {
+        if (x == 0 || x == width - 1) {
+            return true;
+        }
     }
 }
 int death_counter(int n, bool m_death[]) { // Считаем количество умерших клеток
     int c = 0;
     for (int i = 0; i < n; ++i)
-        if (m_death[i]==true) {
+        if (m_death[i] == true) {
             c++;
         }
     return c;

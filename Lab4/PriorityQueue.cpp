a5ededaca4837011cdea908ff7bd7d07c7861a1f
Ayrot2 <91112347+Ayrot2@users.noreply.github.com>
2022-10-15 13:34:32 +0300

    Очередь с приоритетом

---
+++ Lab4/PriorityQueue.cpp
@@ -0,0 +1,131 @@
+﻿#include <iostream>
+using namespace std;
+
+template <typename T>
+struct Element {
+	T element;
+	double priority;
+};
+
+template <typename T>
+void my_swap(T& a, T& b) {
+	T temp;
+	temp = a;
+	a = b;
+	b = temp;
+}
+
+template< typename T>
+struct Comparator {
+	virtual bool operator() (T&, T&) = 0;
+};
+
+struct ComparatorDouble final : Comparator<double>
+{
+	bool operator() (double& lha, double& rha) override {
+		return lha < rha;
+	}
+};
+
+
+template<typename T>
+class MaxHeap
+{
+	Element<T>* harr;
+	int heap_size;
+	Comparator<T>& C;
+public:
+	MaxHeap(Comparator<T>& C) : C(C), heap_size(0), harr(nullptr) {};
+
+	int parent(int i) { return ((i - 1) / 2); }
+
+	void test() {
+		if (C(harr[0].priority, harr[1].priority)) cout << "yeah \n";
+	}
+
+	void push(Element<T> key)
+	{
+		Element<T>* harr2 = new Element<T>[heap_size + 1];
+		for (int j = 0; j < heap_size; j++)
+		{
+			harr2[j] = harr[j];
+		}
+		harr2[heap_size] = key;
+		harr = harr2;
+		heap_size++;
+		int i = heap_size - 1;
+
+		while (i != 0 && C(harr[parent(i)].priority, harr[i].priority))
+		{
+			my_swap(harr[parent(i)], harr[i]);
+			i = parent(i);
+		}
+	}
+
+	void poll()
+	{
+		Element<T>* harr2 = new Element<T>[heap_size - 1];
+		for (int j = 1; j < heap_size; j++)
+		{
+			harr2[j - 1] = harr[j];
+		}
+		harr = harr2;
+		heap_size--;
+		int i = heap_size - 1;
+		while (i != 0 && C(harr[parent(i)].priority, harr[i].priority))
+		{
+			my_swap(harr[parent(i)], harr[i]);
+			i = parent(i);
+		}
+	}
+
+	void output()
+	{
+		for (int i = 0; i < heap_size; i++)
+		{
+			cout << harr[i].element << "\n";
+		}
+	}
+
+	void peek()
+	{
+		cout << harr[0].element << "\n";
+	}
+
+	bool is_empty()
+	{
+		return (heap_size == 0);
+	}
+
+	void free()
+	{
+		delete[] harr;
+	}
+};
+
+
+int main()
+{
+	ComparatorDouble D;
+	MaxHeap<double> heap(D);
+
+	Element<double> one;
+	one.element = 1;
+	one.priority = 3;
+	Element<double> two;
+	two.element = 2;
+	two.priority = 4;
+	Element<double> three;
+	three.element = 3;
+	three.priority = 1;
+
+
+
+	heap.push(three);
+	heap.push(one);
+	heap.push(two);
+	heap.is_empty();
+	//heap.poll();
+	heap.output();
+	heap.free();
+}
